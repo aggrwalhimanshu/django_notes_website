9d7d0afeae630c11adabda20e2a922dcb7da5be1
agarwalhimanshu <aggrwalhimanshu@users.noreply.github.com>
2021-01-14 13:25:39 -0800

    commit on github

---
+++ main/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ToDoList, Item
+from .forms import createList
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+# Create your views here.
+
+def index(response, id):
+	try:
+		ls = ToDoList.objects.get(user=response.user, id=id)
+
+		if response.method == "POST":
+			if response.POST.get("save"):
+				for item in ls.item_set.all():
+					if response.POST.get("c" + str(item.id)) == "clicked":
+						item.complete= True
+					else:
+						item.complete= False
+
+					item.save()
+			elif response.POST.get("newItem"):
+				txt = response.POST.get("new")
+
+				if len(txt)!="":
+					ls.item_set.create(text=txt, complete=False)
+				else:
+					print("Invalid Input")
+	except ObjectDoesNotExist as e:
+		ls=[]
+		print("List not found")
+
+	return render(response, "main/list.html",{"ls":ls})
+
+def home(response):
+	return render(response, "main/home.html",{})
+
+def create(response):
+	if response.method == "POST":
+		form = createList(response.POST)
+
+		if form.is_valid():
+			n=form.cleaned_data["name"]
+			t=ToDoList(name=n, user=response.user)
+			t.save()
+
+		return HttpResponseRedirect("/%i" %t.id)
+	else:
+		form = createList()
+	return render(response, "main/create.html",{"form":form})
+
+def view(response):
+	return render(response, "main/view.html", {})
